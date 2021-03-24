057b748611b767740b16295328db6f9144b0e323
unknown <navinkodam11@gmail.com>
2021-03-24 16:00:15 +0530

    UI updated

--- src/App.js
+++ src/App.js
@@ -1,57 +1,79 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import Button from "@material-ui/core/Button";
+import "./App.css";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
 
 const api = {
   key: "82047cd306c66f2821bcd32c4ca6648d",
-  base: "https://api.openweathermap.org/data/2.5/" 
-}
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
   const handleSearch = () => {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(res => res.json())
-    .then(result =>{ 
-      setWeather(result)
-      setQuery('');
-      console.log(result);
-    });
-  }
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+        setQuery("");
+        console.log(result);
+      });
+  };
 
   return (
     <div className="app">
-      <h1 className="app__h1">Weather App</h1>
-      <div className="container">
-        {/* <InputBox /> */}
-        <div className="inputBox">
-            <input 
-            placeholder="Enter city"
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            />
-            <br />
-            <button
-              onClick={handleSearch}
-            >
-              Search
-            </button>
-        </div>
-        {( typeof weather.main != "undefined") ? (
-        <div className="weather__container">
-          <h2>{weather.name}</h2>
-          <h3>{weather.weather[0].main}</h3>
-          <div className="weatherTemp__container">
-            <p>Temp:{" "}{Math.round(weather.main.temp)}°c</p>
-            <p>Min Temp:{" "}{Math.round(weather.main.temp_min)}°c</p>
-            <p>Max Temp:{" "}{Math.round(weather.main.temp_max)}°c</p>
-          </div>
-        </div>
-        ) : ( ' ' )}
+      <div className="navbar">
+        <MenuIcon />
+        <h3>Weather App</h3>
       </div>
-      
+      <div className="weather__Inputcontainer">
+        <input 
+          type="text" 
+          placeholder="Search City"
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          />
+        <Button 
+          className="weather__searchIcon"
+          onClick={handleSearch}>
+            SEARCH
+        </Button>
+      </div>
+      {typeof weather.main != "undefined" ? (
+      <div className="weather__data">
+        <Card className="weather__card">
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2" className="weather__cardHeader">
+                {weather.name}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Temperature: <span style={{fontWeight: '700', color:'black'}}>{Math.round(weather.main.temp)}°c</span>
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Min Temp: <span style={{fontWeight: '700', color:'black'}}>{Math.round(weather.main.temp_min)}°c</span>
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Max Temp: <span style={{fontWeight: '700', color:'black'}}>{Math.round(weather.main.temp_max)}°c</span>
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Country: <span style={{fontWeight: '700', color:'black'}}>{(weather.sys.country)}</span>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+      ) : (
+        " "
+      )}
     </div>
   );
 }
